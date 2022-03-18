6b8ba0b3247bb246915fc1f8ddbe635291dc65cd
Miguel <miguel.gallardof@icloud.com>
2022-03-17 19:06:42 -0500

    Homework JS II

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -210,6 +210,7 @@ function deEuroAdolar(euro) {
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
   return euro * 1.2;
+  // no se pone , para separa decimales
 }
 
 function esVocal(letra) {
